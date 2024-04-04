e5a9ebe8151addda4acafe946968da454bda7c96
Apoorva Saxena <apoorvasaxena@Apoorvas-MacBook-Air.local>
2024-04-04 13:40:49 +0530

    vendi begins

---
+++ js/scrollToBottom.js
@@ -0,0 +1,18 @@
+function scrollToBottom() {
+    window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: 'smooth'
+    });
+}
+
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1200, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
